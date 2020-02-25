7f32114e389ce81ab2a181ac0c36857dcf9905e0
Simon Woldemichael <swoldemichael@acm.org>
2020-02-24 23:45:37 -0600

    Update main routine to initialize cloudwatch client and xray configuration
    Use closure pattern for handler

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/service/cloudwatch"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/aws/aws-xray-sdk-go/xray"
 	log "github.com/sirupsen/logrus"
@@ -16,10 +17,20 @@ func main() {
 		log.Fatalf("Error while attempting to assume cross account role: %v\n", err)
 		return
 	}
-	svc := ec2.New(sess)
-	xray.AWS(svc.Client)
 
-	h, err := lib.FunctionContainer(svc)
+	ec2Svc := ec2.New(sess)
+	cwSvc := cloudwatch.New(sess)
+	if err := xray.Configure(xray.Config{LogLevel: "trace"}); err != nil {
+		log.Fatalf("Error configuring X-Ray: %v\n", err)
+		return
+	}
+
+	xray.AWS(ec2Svc.Client)
+	xray.AWS(cwSvc.Client)
+	log.Info("Enabled request tracing on EC2 and CloudWatch API clients")
+
+	f := lib.NewFunctionContainer(ec2Svc, cwSvc, lib.Production)
+	h, err := f.GetHandler()
 	if err != nil {
 		log.Fatalf("Error initializing Lambda environment: %v\n", err)
 		return
