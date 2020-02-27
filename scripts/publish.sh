f3024481653c9187cca60a32e5896883107a4462
Simon Woldemichael <swoldemichael@acm.org>
2020-02-26 22:28:51 -0600

    Fix typo

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,7 +1,7 @@
 echo "On branch `basename $CODEBUILD_WEBHOOK_HEAD_REF`"
-if [ "basename $CODEBUILD_WEBHOOK_HEAD_REF" = X"master" ]
+if [ "`basename $CODEBUILD_WEBHOOK_HEAD_REF`" = "master" ]
 then 
-    make sam-publish; 
+    make sam-publish
 else
     echo Skipping publish
 fi
